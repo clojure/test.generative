0db44b74b05cf23ae97085b673c994b16bc63fcc
Tom Faulhaber <git_com@infolace.com>
2016-06-06 12:47:25 -0700

    Autodoc commit for master/0baf290c

--- index-0.5.3.clj
+++ index-0.5.3.clj
@@ -1,14 +1,13 @@
 {:namespaces
  ({:doc nil,
    :name "clojure.test.generative",
-   :wiki-url
-   "http://clojure.github.com/test.generative/clojure.test.generative-api.html",
+   :wiki-url "http://clojure.github.io/test.generative/index.html",
    :source-url
    "https://github.com/clojure/test.generative/blob/fe4922b988df76fff56e287eec41e7d5183b7a40/src/main/clojure/clojure/test/generative.clj"}
   {:doc nil,
    :name "clojure.test.generative.runner",
    :wiki-url
-   "http://clojure.github.com/test.generative/clojure.test.generative.runner-api.html",
+   "http://clojure.github.io/test.generative/index.html#clojure.test.generative.runner",
    :source-url
    "https://github.com/clojure/test.generative/blob/635f98e948e6ec585fb575809b1d62d9593ae402/src/main/clojure/clojure/test/generative/runner.clj"}),
  :vars
@@ -25,7 +24,7 @@
    "Defines a function named name that expects args. The defined\nfunction binds '%' to the result of calling fn-to-test with args,\nand runs validator-body forms (if any), which have access to both\nargs and %. The defined function.\n\nArgs must have type hints (i.e. :tag metadata), which are\ninterpreted as instructions for generating test input\ndata. Unquoted names in type hints are resolved in the\nc.t.g.generators namespace, which has generator functions for\ncommon Clojure data types. For example, the following argument list\ndeclares that 'seed' is an int, and that 'iters' is an int in the\nuniform distribution from 1 to 100:\n\n    [^int seed ^{:tag (uniform 1 100)} iters]\n\nBackquoted names in an argument list are resolved in the current\nnamespace, allowing arbitrary generators, e.g.\n\n    [^{:tag `scary-word} word]\n\nThe function c.t.g.runner/run-iter takes a var naming a test, and runs\na single test iteration, generating inputs based on the arg type hints.",
    :namespace "clojure.test.generative",
    :wiki-url
-   "http://clojure.github.com/test.generative//clojure.test.generative-api.html#clojure.test.generative/defspec"}
+   "http://clojure.github.io/test.generative//index.html#clojure.test.generative/defspec"}
   {:raw-source-url
    "https://github.com/clojure/test.generative/raw/635f98e948e6ec585fb575809b1d62d9593ae402/src/main/clojure/clojure/test/generative/runner.clj",
    :name "-main",
@@ -38,7 +37,7 @@
    :doc "Command line entry point. Calls System.exit!",
    :namespace "clojure.test.generative.runner",
    :wiki-url
-   "http://clojure.github.com/test.generative//clojure.test.generative-api.html#clojure.test.generative.runner/-main"}
+   "http://clojure.github.io/test.generative//index.html#clojure.test.generative.runner/-main"}
   {:raw-source-url
    "https://github.com/clojure/test.generative/raw/635f98e948e6ec585fb575809b1d62d9593ae402/src/main/clojure/clojure/test/generative/runner.clj",
    :name "config",
@@ -51,7 +50,7 @@
    :doc "Returns runner configuration derived from system properties.",
    :namespace "clojure.test.generative.runner",
    :wiki-url
-   "http://clojure.github.com/test.generative//clojure.test.generative-api.html#clojure.test.generative.runner/config"}
+   "http://clojure.github.io/test.generative//index.html#clojure.test.generative.runner/config"}
   {:raw-source-url
    "https://github.com/clojure/test.generative/raw/635f98e948e6ec585fb575809b1d62d9593ae402/src/main/clojure/clojure/test/generative/runner.clj",
    :name "dir-tests",
@@ -64,7 +63,7 @@
    :doc "Returns all tests in dirs",
    :namespace "clojure.test.generative.runner",
    :wiki-url
-   "http://clojure.github.com/test.generative//clojure.test.generative-api.html#clojure.test.generative.runner/dir-tests"}
+   "http://clojure.github.io/test.generative//index.html#clojure.test.generative.runner/dir-tests"}
   {:raw-source-url
    "https://github.com/clojure/test.generative/raw/635f98e948e6ec585fb575809b1d62d9593ae402/src/main/clojure/clojure/test/generative/runner.clj",
    :name "inputs",
@@ -78,7 +77,7 @@
    "For interactive use.  Returns an infinite sequence of inputs for\na test.",
    :namespace "clojure.test.generative.runner",
    :wiki-url
-   "http://clojure.github.com/test.generative//clojure.test.generative-api.html#clojure.test.generative.runner/inputs"}
+   "http://clojure.github.io/test.generative//index.html#clojure.test.generative.runner/inputs"}
   {:raw-source-url
    "https://github.com/clojure/test.generative/raw/635f98e948e6ec585fb575809b1d62d9593ae402/src/main/clojure/clojure/test/generative/runner.clj",
    :name "prf",
@@ -91,7 +90,7 @@
    :doc "Print and flush.",
    :namespace "clojure.test.generative.runner",
    :wiki-url
-   "http://clojure.github.com/test.generative//clojure.test.generative-api.html#clojure.test.generative.runner/prf"}
+   "http://clojure.github.io/test.generative//index.html#clojure.test.generative.runner/prf"}
   {:raw-source-url
    "https://github.com/clojure/test.generative/raw/635f98e948e6ec585fb575809b1d62d9593ae402/src/main/clojure/clojure/test/generative/runner.clj",
    :name "run",
@@ -105,7 +104,7 @@
    "Designed for interactive use.  Prints results to *out* and throws\non first failure encountered.",
    :namespace "clojure.test.generative.runner",
    :wiki-url
-   "http://clojure.github.com/test.generative//clojure.test.generative-api.html#clojure.test.generative.runner/run"}
+   "http://clojure.github.io/test.generative//index.html#clojure.test.generative.runner/run"}
   {:raw-source-url
    "https://github.com/clojure/test.generative/raw/635f98e948e6ec585fb575809b1d62d9593ae402/src/main/clojure/clojure/test/generative/runner.clj",
    :name "run-suite",
@@ -118,7 +117,7 @@
    :doc "Designed for test suite use.",
    :namespace "clojure.test.generative.runner",
    :wiki-url
-   "http://clojure.github.com/test.generative//clojure.test.generative-api.html#clojure.test.generative.runner/run-suite"}
+   "http://clojure.github.io/test.generative//index.html#clojure.test.generative.runner/run-suite"}
   {:raw-source-url
    "https://github.com/clojure/test.generative/raw/635f98e948e6ec585fb575809b1d62d9593ae402/src/main/clojure/clojure/test/generative/runner.clj",
    :name "serialized",
@@ -132,4 +131,4 @@
    "Returns a function that calls f for side effects, async,\nserialized by an agent",
    :namespace "clojure.test.generative.runner",
    :wiki-url
-   "http://clojure.github.com/test.generative//clojure.test.generative-api.html#clojure.test.generative.runner/serialized"})}
+   "http://clojure.github.io/test.generative//index.html#clojure.test.generative.runner/serialized"})}
